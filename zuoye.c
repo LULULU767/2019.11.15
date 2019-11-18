fad925f410170689ff7e3212ac67c2d4cf221664
unknown <you@example.com>
2019-11-18 20:20:22 +0800

    https://github.com/LULULU767/2019.11.15.git

---
+++ zuoye.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (void)
+{
+    int n,i,sum=0,product=1;
+    printf("Enter n：");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        product=product*i;
+        sum=product+sum;
+    }
+    printf("sum is %d.",sum);
+    return 0;  
+}
